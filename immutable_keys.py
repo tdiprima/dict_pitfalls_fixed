3aba83e02a85e6e6d12871652bd0189fce015bcb
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2025-10-18 07:10:08 -0400

    Clarify dictionary key immutability with comments
    Added comment to clarify immutability of dictionary keys.

--- immutable_keys.py
+++ immutable_keys.py
@@ -1,4 +1,6 @@
 # Demonstrates that dictionary keys must be immutable, using a tuple instead of a list
 
+# my_dict = {[1, 2]: "greeting"}  # ❌ Wrong
+
 my_dict = {(1, 2): "greeting"}
 print(my_dict[(1, 2)])  # Outputs: greeting
