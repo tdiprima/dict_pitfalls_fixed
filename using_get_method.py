b21ec18361cc0957e8492ad52132d886850dbdf0
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2025-10-18 07:12:23 -0400

    Add example of using .get() method in Python
    Demonstrate the use of the .get() method for accessing dictionary keys safely with default values.

---
+++ using_get_method.py
@@ -0,0 +1,6 @@
+# Illustrates the .get() method for safely accessing keys with defaults
+# Avoids "KeyError"
+
+data = {"name": "Bear"}
+print(data.get("height"))       # None
+print(data.get("height", 180))  # 180
