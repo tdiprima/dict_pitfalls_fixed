7c3122d7d47d4aab6ecceea3adbe51e60e422ec8
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2026-02-04 10:34:05 -0500

    tell Ruff (and flake8) to skip

--- src/overwriting_keys.py
+++ src/overwriting_keys.py
@@ -1,4 +1,4 @@
 # Shows how assigning the same key multiple times overwrites the previous value
 
-my_dict = {"letter": 1, "letter": 3}
+my_dict = {"letter": 1, "letter": 3}  # noqa: F601
 print(my_dict)  # {'letter': 3}
